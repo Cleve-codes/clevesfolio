d9e26a75430ca230d13ee140b2d9ff6e8b2a798b
Cleve-codes <clevemomanyi@gmail.com>
2024-02-15 23:13:29 +0300

    animations

--- app/page.js
+++ app/page.js
@@ -13,18 +13,46 @@ import { useEffect, useRef } from 'react';
 import Footer from "@/sections/Footer/Footer";
 // import Footer from "@/components/Footer/Footer";
 
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+import { useGSAP } from '@gsap/react'
+
 
 // import { useEffect, useRef } from 'react';
 export default function Page() {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+
+  /* GSAP Animation to make the left section scale up and the right section scale down when the About section
+  comes into view */
+
+
+  useGSAP(() => {
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#about',
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+        // markers: true,
+        toggleActions: "play none none reverse"
+      }
+    });
+
+    tl.fromTo('#left-section', { scale: 1 }, { scale: 1.2, duration: 1 })
+
+  }, [])
+
 
   return (
     <main className="flex bg-[#1d1b19]">
-      <div className="border-red"  >
+      <div className="border-red" id="left-section"  >
         <div className="flex px-28 flex-col min-w-[1/2]" >
           <div className="min-h-screen w-full flex flex-row">
             <Home />
           </div>
-          <div>
+          <div id="about" >
             <About />
           </div>
           <div>
--- sections/RightSection/Right.jsx
+++ sections/RightSection/Right.jsx
@@ -1,44 +1,74 @@
-import Navbar from '@/components/Navbar/Navbar'
-import React from 'react'
+import Navbar from "@/components/Navbar/Navbar";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { buttonVariants } from "@/components/ui/button";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import dynamic from "next/dynamic";
+import mask from "@/public/assets/mask.svg";
 
-import './Right.css'
+import "./Right.css";
 
 const Right = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  /* GSAP ANimation to scale down the image and its 2 frames when the about section is reached  */
+
   return (
     <div className="right-section">
-        <div>
-          <Navbar />
-        </div>
-        <div className="detail">
-          <div className="imgFrame1">
-            <div className="imgFrame2">
-              <div className="image">
-                <Image className="hero-img" src='/assets/asset 20.jpg' width={487} height={487} alt="hero"></Image>
-              </div>
-              <div className="intro">
-                <p>Hi THERE! I AM</p>
-                <br />
-                <h2>[<span>Web Developer</span>]</h2>
-              </div>
-            </div>
-          </div>
+      <div>
+        <Navbar />
+      </div>
+      <div className="detail">
+        {/* <div className="imgFrame1"> */}
+          {/* <div className="imgFrame2"> */}
+          <div
+          className="image"
+           >
 
-          <div className="text-center ">
-            <Link href="#contact" className={buttonVariants({
-              variant: 'link',
-              className: 'outline-none'
-            })}>
-              <p className='text'>
-                I&apos;m available for a freelance job. Hire me
-              </p>
-            </Link>
-          </div>
+            <div >
+              <Image
+                className="hero-img"
+                // src="/assets/asset 20.jpg"
+                src={mask}
+                width={487}
+                height={487}
+                alt="hero"
+                ></Image>
+            </div>
+
+            <div className="intro">
+              <p>Hi THERE! I AM</p>
+              <br />
+              <h2>
+                [<span>Web Developer</span>]
+              </h2>
+        </div>
+
+            </div>
+
+
+        <div className="text-center ">
+          <Link
+            href="#contact"
+            className={buttonVariants({
+              variant: "link",
+              className: "outline-none",
+            })}
+          >
+            <p className="text">
+              I&apos;m available for a freelance job. Hire me
+            </p>
+          </Link>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Right
+// export default Right;
+export default dynamic(() => Promise.resolve(Right), {
+  ssr: false,
+});
